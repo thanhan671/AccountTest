0ed08a42afd9e16d6ee3c29b9c24ca7ce6950d1e
Nguyễn Thanh An <thanhannguyen67@gmail.com>
2021-11-11 08:51:03 +0700

    Initial commit

---
+++ app/src/main/java/com/thanhan/accounttest/FlashActivity.java
@@ -0,0 +1,24 @@
+package com.thanhan.accounttest;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class FlashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_flash);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent i = new Intent(FlashActivity.this,MainActivity.class);
+                startActivity(i);
+                finish();
+            }
+        }, 3000);
+    }
+}
